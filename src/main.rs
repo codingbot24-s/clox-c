755918fb4e3171e3151dc2de9d55acccf9f2483f
pcodefromWIN <primecoder8@gmail.com>
2025-12-30 08:53:36 +0000

    add getting offset-

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,54 @@
-
-
+#[derive(Debug)]
 enum OPCODE {
-    OPRETURN
+    OPRETURN,
 }
 
 struct Chunk {
     // vec of op_code
-    code:Vec<OPCODE>,
-    index:usize,
+    code: Vec<OPCODE>,
+    index: usize,
 }
 
 impl Chunk {
-    fn new () -> Self {
-        Chunk { 
-            code:Vec::new(),
-            index:0,
+    fn new() -> Self {
+        Chunk {
+            code: Vec::new(),
+            index: 0,
         }
     }
 
-    fn write_chunk (&mut self,code:OPCODE) {
-        self.code[self.index] = code;
-        self.index+=1;
+    fn write_chunk(&mut self, code: OPCODE) {
+        self.code.push(code);
+        self.index += 1;
     }
-    
-    fn disassemble_chunk (&self,name:&str) {
-        println!("{}",name);
-        // TODO: (pcode) -> implement the disassmeble the chunk
+    // we need the index also
+    fn disassemble_chunk(&self, name: &str) {
+        println!("== {} == ", name);
+        let mut offset = 0;
+        while offset < self.code.len() {
+            if let Ok(of) = self.disassemble_instruction(offset) {
+                offset = of
+            }
+        }
+        
+    }
+
+    fn disassemble_instruction(&self,offset:usize) -> Result<usize,()> {
+        print!("{:04} ",offset);
+        if let Some(ins) = self.code.get(offset) {
+            match ins {
+                OPCODE::OPRETURN => {
+                    Ok(self.simple_instruction(ins, offset))
+                }
+            }
+        }else {
+            Err(eprintln!("error getting the next instruction"))
+        }
+    }
+
+    fn simple_instruction(&self, v: &OPCODE, offset: usize) -> usize {
+        println!("{:?}", v);
+        offset + 1
     }
 }
 
